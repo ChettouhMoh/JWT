30f7975a60f9d4a3e07917d09518fa8b2fe78b4b
ChettouhMoh <123198439+ChettouhMoh@users.noreply.github.com>
2024-10-09 19:45:49 +0100

    added craco for aliasses and react router

---
+++ frontend/craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  webpack: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@rtk": path.resolve(__dirname, "src/rtk"),
+      "@api": path.resolve(__dirname, "src/api"),
+    },
+  },
+};
