1e1c72d4f079283daaa443d0ac31b9c107839184
ChettouhMoh <123198439+ChettouhMoh@users.noreply.github.com>
2024-10-12 13:57:54 +0100

    configured react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import Homepage from "@pages/Homepage";
 import "./App.css";
-import SignUp from "./components/sign-up/SignUp";
-
+import SignUp from "@components/sign-up/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <SignUp />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
