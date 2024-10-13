111b4ee64d01ac9bd3ac0dae874dcc1d4562ed17
ChettouhMoh <123198439+ChettouhMoh@users.noreply.github.com>
2024-10-13 18:09:29 +0100

    reconfigured Users model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  role: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, required: true },
+  bio: { type: String },
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }], // List of users following this user
+  following: [{ type: Schema.Types.ObjectId, ref: "User" }], // List of users this user is following
+  blogs: [{ type: Schema.Types.ObjectId, ref: "Blog" }], // List of blog posts created by this user
+  createdAt: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("User", userSchema);
