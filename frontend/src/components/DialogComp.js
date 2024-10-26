170c1e87bd7d7bf2085e08fd2c48585f0dda0798
ChettouhMoh <123198439+ChettouhMoh@users.noreply.github.com>
2024-10-26 01:18:13 +0100

    added self created Dialog component

--- frontend/src/components/DialogComp.js
+++ frontend/src/components/DialogComp.js
@@ -1,67 +1,167 @@
-// import * as React from "react";
-// import {
-//   Dialog,
-//   DialogActions,
-//   DialogContent,
-//   DialogTitle,
-//   Button,
-//   TextField,
-// } from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
+import { Dialog, Container, Button, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
-// export const DialogComp = () => {
-//   return (
-//     <Dialog
-//       openDialog={openDialog}
-//       onClose={handleClose}
-//       fullWidth
-//       maxWidth="sm" // Makes the dialog small and centered
-//       PaperProps={{
-//         sx: {
-//           backgroundColor: "#424242", // Dark background for the dialog
-//           color: "#fff", // White text color for dark mode
-//         },
-//       }}
-//     >
-//       <DialogTitle>{"Add a New Post"}</DialogTitle>
-//       <DialogContent>
-//         <TextField
-//           autoFocus
-//           margin="dense"
-//           id="post"
-//           label="Post"
-//           type="text"
-//           fullWidth
-//           multiline
-//           rows={4}
-//           variant="outlined"
-//           sx={{
-//             backgroundColor: "#616161", // Input field background for dark mode
-//             color: "#fff",
-//             "& label.Mui-focused": {
-//               color: "#00bcd4", // Focused input label color
-//             },
-//             "& .MuiOutlinedInput-root": {
-//               "& fieldset": {
-//                 borderColor: "#bdbdbd", // Border color for input
-//               },
-//               "&:hover fieldset": {
-//                 borderColor: "#00bcd4", // Border color on hover
-//               },
-//               "&.Mui-focused fieldset": {
-//                 borderColor: "#00bcd4", // Border color when focused
-//               },
-//             },
-//           }}
-//         />
-//       </DialogContent>
-//       <DialogActions>
-//         <Button onClick={handleClose} sx={{ color: "#bdbdbd" }}>
-//           Cancel
-//         </Button>
-//         <Button onClick={handleClose} sx={{ color: "#00bcd4" }}>
-//           Add Post
-//         </Button>
-//       </DialogActions>
-//     </Dialog>
-//   );
-// };
+const AddPostDialog = ({ openDialog, handleClose }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    if (file) {
+      const fileSize = (file.size / (1024 * 1024)).toFixed(2);
+      setSelectedImage(`${file.name} - ${fileSize} MB`);
+    }
+  };
+
+  return (
+    <Dialog
+      open={openDialog}
+      onClose={handleClose}
+      fullWidth
+      PaperProps={{
+        sx: {
+          borderRadius: "24px !important",
+          backgroundColor: "hsl(220deg 35% 3% / 98%)",
+          backgroundImage: "none",
+          color: "#fff",
+          overflow: "hidden",
+          width: "475px",
+          padding: "20px",
+        },
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "24px",
+          height: "24px",
+          borderRadius: "50%",
+          border: "none",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          right: "17px",
+          cursor: "pointer",
+          top: "17px",
+          color: "#b3b3b3",
+          backgroundColor: "#363639",
+        }}
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="10px" />
+      </div>
+      <Container
+        sx={{
+          maxHeight: "calc(100vh - 200px)",
+          overflow: "auto",
+          padding: "0 10px",
+          margin: "10px 0 20px 0",
+          position: "relative",
+        }}
+      >
+        <h3 style={{ fontFamily: "Inter" }}>Write your thoughts.</h3>
+        <label htmlFor="image-upload">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              height: "100px",
+              color: selectedImage ? "#fff" : "#5e5e5f",
+              borderRadius: "14px",
+              backgroundColor: "#2b2b2b38",
+              cursor: "pointer",
+              border: selectedImage
+                ? "1px solid #0097a7"
+                : "1px dashed #5e5e5f",
+              transition: "background-color 0.3s, border 0.3s",
+              "&:hover": {
+                backgroundColor: "#2b2b2b88",
+              },
+            }}
+          >
+            <AddPhotoAlternateIcon sx={{ marginTop: "22px" }} />
+            <h5 style={{ marginTop: "6px", fontFamily: "Inter" }}>
+              {selectedImage || "upload image"}
+            </h5>
+          </div>
+          <input
+            id="image-upload"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+          />
+        </label>
+        <div
+          style={{
+            fontFamily: "Inter",
+            fontSize: "16px",
+            margin: "10px 0 5px 0",
+          }}
+        >
+          Title
+        </div>
+        <input
+          style={{
+            outline: "none",
+            border: "none",
+            borderBottom: "1px solid #292828",
+            width: "100%",
+            height: "30px",
+            background: "none",
+          }}
+        />
+        <div
+          style={{
+            fontFamily: "Inter",
+            fontSize: "16px",
+            margin: "18px 0 5px 0",
+          }}
+        >
+          Content
+        </div>
+        <textarea
+          style={{
+            outline: "none",
+            border: "none",
+            borderBottom: "1px solid #292828",
+            width: "100%",
+            height: "30px",
+            background: "none",
+            marginBottom: "80px",
+            resize: "none",
+            overflow: "hidden",
+          }}
+          onInput={(e) => {
+            e.target.style.height = "auto";
+            e.target.style.height = `${e.target.scrollHeight}px`;
+          }}
+        />
+
+        <Button
+          sx={{
+            position: "absolute",
+            bottom: "0px",
+            right: "25px",
+            background: "#292828",
+            borderRadius: "20px",
+            padding: "0 15px",
+            color: "#fff",
+            "&:hover": {
+              backgroundColor: "#0097a7",
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Container>
+    </Dialog>
+  );
+};
+
+export default AddPostDialog;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -11,23 +11,14 @@ import { useSelector } from "react-redux";
 import getBlogTheme from "../components/sign-up/theme/getSignUpTheme";
 import HomepageSkeleton from "../components/HomepageSkeleton";
 import { SucceedAlert } from "../components/SucceedAlert";
-import { DialogComp } from "../components/DialogComp";
+import AddPostDialog from "../components/DialogComp";
 import ErrorAlert from "../components/ErrorAlert";
-import {
-  Fab,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Button,
-  TextField,
-} from "@mui/material";
+import { Fab, Dialog, Button } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 
 const Homepage = () => {
   const blogTheme = createTheme(getBlogTheme("dark"));
   const [openDialog, setOpenDialog] = React.useState(false);
-  console.log(openDialog);
   const { msg, error, loading } = useSelector((state) => state.auth);
 
   const handleClickOpen = () => {
@@ -42,7 +33,7 @@ const Homepage = () => {
     <TemplateFrame mode={"dark"}>
       <ThemeProvider theme={blogTheme}>
         <CssBaseline enableColorScheme />
-        {/* {loading ? (
+        {loading ? (
           <HomepageSkeleton />
         ) : (
           <>
@@ -63,7 +54,7 @@ const Homepage = () => {
             </Container>
             <Footer />
           </>
-        )} */}
+        )}
         <Fab
           color="default"
           aria-label="edit"
@@ -82,90 +73,7 @@ const Homepage = () => {
           <EditIcon />
         </Fab>
         {openDialog ? (
-          <Dialog
-            open={openDialog}
-            onClose={handleClose}
-            fullWidth
-            maxWidth="sm"
-            PaperProps={{
-              sx: {
-                backgroundColor: "#333", // Darker background for dialog
-                color: "#fff",
-                borderRadius: "12px", // Rounded corners for a modern look
-              },
-            }}
-          >
-            <DialogTitle sx={{ color: "#e0f7fa" }}>Add a New Post</DialogTitle>
-            <DialogContent>
-              {/* TextField */}
-              <TextField
-                autoFocus
-                margin="dense"
-                id="post"
-                label="Write your post..."
-                type="text"
-                fullWidth
-                multiline
-                rows={4}
-                variant="outlined"
-                InputProps={{
-                  sx: {
-                    backgroundColor: "#424242", // Subtle dark background for the input
-                    color: "#fff",
-                  },
-                }}
-                InputLabelProps={{
-                  sx: {
-                    color: "#bdbdbd", // Label color in dark mode
-                  },
-                }}
-                sx={{
-                  "& label.Mui-focused": {
-                    color: "#80deea", // Teal focus color for label
-                  },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "#bdbdbd", // Initial border color
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "#80deea", // Teal border on hover
-                    },
-                    "&.Mui-focused fieldset": {
-                      borderColor: "#80deea", // Teal border on focus
-                    },
-                  },
-                }}
-              />
-            </DialogContent>
-            <DialogActions sx={{ padding: "16px" }}>
-              {/* Cancel Button */}
-              <Button
-                onClick={handleClose}
-                sx={{
-                  color: "#f44336", // Red for cancel button
-                  backgroundColor: "rgba(255, 68, 68, 0.1)", // Soft red background
-                  "&:hover": {
-                    backgroundColor: "rgba(255, 68, 68, 0.2)", // Darker red on hover
-                  },
-                }}
-              >
-                Cancel
-              </Button>
-              {/* Add Post Button */}
-              <Button
-                onClick={handleClose}
-                sx={{
-                  color: "#00bcd4", // Teal for submit button
-                  backgroundColor: "rgba(0, 188, 212, 0.1)", // Soft teal background
-                  "&:hover": {
-                    backgroundColor: "rgba(0, 188, 212, 0.2)", // Darker teal on hover
-                  },
-                }}
-              >
-                Add Post
-              </Button>
-            </DialogActions>
-          </Dialog>
+          <AddPostDialog openDialog={openDialog} handleClose={handleClose} />
         ) : null}
 
         {!!msg ? <SucceedAlert message={msg} /> : null}
